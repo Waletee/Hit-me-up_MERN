ed6e18d78af6a8e3cdbd17b6a588614ad1cac01b
Taiwo Abiodun <taiwobiodun@gmail.com>
2024-04-29 14:39:44 +0100

    sort out base URL issues

--- client/src/api/AuthRequests.js
+++ client/src/api/AuthRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-
-const API = axios.create({ baseURL: 'https://hit-me-up-api.onrender.com' });
+const baseurl = process.env.BASE_URL;
+const API = axios.create({ baseURL: baseurl });
 
 export const logIn= (formData)=> API.post('/auth/login', formData);
 export const signUp = (formData) => API.post('/auth/register', formData);
