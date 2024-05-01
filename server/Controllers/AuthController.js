e638c470b0079245f8648f602c92dfc36cf6f006
Taiwo Abiodun <taiwobiodun@gmail.com>
2024-05-01 15:40:51 +0100

    resolve npm install error in server package json

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,21 +1,22 @@
 import UserModel from "../Models/UserModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
 
 // Register new user
 export const registerUser = async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hashedPass = await bcrypt.hash(req.body.password, salt);
-  req.body.password = hashedPass
+  req.body.password = hashedPass;
   const newUser = new UserModel(req.body);
-  const {username} = req.body
+  const { username } = req.body;
   try {
     // Check if username already exist
     const oldUser = await UserModel.findOne({ username });
 
     if (oldUser)
-      return res.status(400).json({ message: "Username already taken use another one" });
+      return res
+        .status(400)
+        .json({ message: "Username already taken use another one" });
 
     const user = await newUser.save();
     const token = jwt.sign(
@@ -31,12 +32,11 @@ export const registerUser = async (req, res) => {
 
 // Login user
 export const loginUser = async (req, res) => {
-  const {username, password} = req.body;
+  const { username, password } = req.body;
   try {
-    const user = await UserModel.findOne({username: username})
-    if(user)
-    {
-      const validity = await bcrypt.compare(password, user.password)
+    const user = await UserModel.findOne({ username: username });
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
       if (!validity) {
         res.status(400).json("wrong password");
       } else {
@@ -44,7 +44,7 @@ export const loginUser = async (req, res) => {
           { username: user.username, id: user._id },
           process.env.JWT_KEY,
           { expiresIn: "1h" }
-        )
+        );
         res.status(200).json({ user, token });
       }
     } else {
