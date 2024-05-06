b5b026a8ca5a987566b238431a946d7ed8cc6caa
Taiwo Abiodun <taiwobiodun@gmail.com>
2024-05-06 10:03:22 +0100

    Adding cloudinary to handle image upload on backend

--- server/Routes/UploadRoute.js
+++ server/Routes/UploadRoute.js
@@ -1,24 +1,39 @@
-import express from 'express'
-const router = express.Router()
-import multer from 'multer'
+import express from "express";
+const router = express.Router();
+const cloudinary = require("../cloudinary");
+import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "Public/images");
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.body.name);
-    },
-  });
+  destination: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 const upload = multer({ storage: storage });
 
-
 router.post("/", upload.single("file"), (req, res) => {
-    try {
+  cloudinary.uploader.upload(req.file.path, function (err, result) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        success: false,
+        message: "Error",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Uploaded!",
+      data: result,
+    });
+  });
+  /*try {
       return res.status(200).json("File uploaded successfully");
     } catch (error) {
       console.error(error);
-    }
-  });
+    }*/
+});
 
-export default router
+export default router;
